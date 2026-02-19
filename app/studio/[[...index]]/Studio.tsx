a88bd7a20783246030b5bba402f35daef1079ded
victorfreire <victorfreire21@gmail.com>
2026-02-19 16:29:29 -0500

    fix(studio): add explicit React imports to fix ReferenceError in production

--- app/studio/[[...index]]/Studio.tsx
+++ app/studio/[[...index]]/Studio.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import React from "react";
+
 import { NextStudio } from "next-sanity/studio";
 import config from "../../../sanity/sanity.config";
 
--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Studio from "./Studio";
 
 export const dynamic = "force-static";
